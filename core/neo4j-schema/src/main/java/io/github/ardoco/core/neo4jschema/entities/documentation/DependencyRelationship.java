dcdc4ecc6794b5bbe70169c7f9905894d6ffd7fd
Dominik Fuchß <dominik.fuchss@kit.edu>
2026-02-09 14:19:38 +0100

    Reduce warnings.

--- core/neo4j-schema/src/main/java/io/github/ardoco/core/neo4jschema/entities/documentation/DependencyRelationship.java
+++ core/neo4j-schema/src/main/java/io/github/ardoco/core/neo4jschema/entities/documentation/DependencyRelationship.java
@@ -1,15 +1,17 @@
 /* Licensed under MIT 2026. */
 package io.github.ardoco.core.neo4jschema.entities.documentation;
 
-import org.springframework.data.neo4j.core.schema.RelationshipId;
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.RelationshipProperties;
 import org.springframework.data.neo4j.core.schema.TargetNode;
 
 @RelationshipProperties
 public class DependencyRelationship {
 
-    @RelationshipId
-    private Long id;
+    @Id
+    @GeneratedValue
+    private String id;
 
     private String dependencyType; // Stores DependencyTag.name()
 
