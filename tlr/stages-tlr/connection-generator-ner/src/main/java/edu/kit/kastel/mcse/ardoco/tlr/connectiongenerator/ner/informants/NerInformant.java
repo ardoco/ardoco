e07e38f0955583e1d736a7a272469b3cb0810b85
Johannes von Geisau <johannes.von.geisau@t-online.de>
2026-06-24 15:47:35 +0200

    use LLM cache for Artemis inconsistency evaluation

--- tlr/stages-tlr/connection-generator-ner/src/main/java/edu/kit/kastel/mcse/ardoco/tlr/connectiongenerator/ner/informants/NerInformant.java
+++ tlr/stages-tlr/connection-generator-ner/src/main/java/edu/kit/kastel/mcse/ardoco/tlr/connectiongenerator/ner/informants/NerInformant.java
@@ -53,7 +53,7 @@ public class NerInformant extends Informant {
         var text = DataRepositoryHelper.getSimpleText(dataRepository);
         SoftwareArchitectureDocumentation sad = new SoftwareArchitectureDocumentation(text.getText());
 
-        var chatModel = llm.createUncached();
+        var chatModel = llm.create();
 
         var modelStatesData = DataRepositoryHelper.getModelStatesData(dataRepository);
         for (var metamodel : modelStatesData.getMetamodels()) {
