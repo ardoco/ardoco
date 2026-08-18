093db720d2e0c86a79276d28510c810da1f8bf22
4KevR <50447092+4KevR@users.noreply.github.com>
2026-08-18 14:07:38 +0200

    change ANTLR python grammar to support python 3.14 expressions

--- tlr/stages-tlr/model-provider/src/main/java/edu/kit/kastel/mcse/ardoco/tlr/models/connectors/generators/antlr/extraction/python3/Python3ElementExtractor.java
+++ tlr/stages-tlr/model-provider/src/main/java/edu/kit/kastel/mcse/ardoco/tlr/models/connectors/generators/antlr/extraction/python3/Python3ElementExtractor.java
@@ -12,10 +12,11 @@ import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.TerminalNode;
 
-import edu.kit.kastel.mcse.ardoco.tlr.models.antlr4.python3.Python3Lexer;
-import edu.kit.kastel.mcse.ardoco.tlr.models.antlr4.python3.Python3Parser;
-import edu.kit.kastel.mcse.ardoco.tlr.models.antlr4.python3.Python3Parser.File_inputContext;
+import edu.kit.kastel.mcse.ardoco.tlr.models.antlr4.python3.PythonLexer;
+import edu.kit.kastel.mcse.ardoco.tlr.models.antlr4.python3.PythonParser;
+import edu.kit.kastel.mcse.ardoco.tlr.models.antlr4.python3.PythonParser.File_inputContext;
 import edu.kit.kastel.mcse.ardoco.tlr.models.connectors.generators.antlr.elements.ClassElement;
 import edu.kit.kastel.mcse.ardoco.tlr.models.connectors.generators.antlr.elements.Element;
 import edu.kit.kastel.mcse.ardoco.tlr.models.connectors.generators.antlr.elements.ElementIdentifier;
@@ -70,7 +71,7 @@ public class Python3ElementExtractor extends ElementExtractor {
     @Override
     protected CommonTokenStream buildTokens(Path absoluteFile, Path relativeFile) throws IOException {
         CharStream charStream = CharStreams.fromReader(Files.newBufferedReader(absoluteFile), relativeFile.toString());
-        Python3Lexer lexer = new Python3Lexer(charStream);
+        PythonLexer lexer = new PythonLexer(charStream);
         return new CommonTokenStream(lexer);
     }
 
@@ -82,20 +83,20 @@ public class Python3ElementExtractor extends ElementExtractor {
     }
 
     private File_inputContext buildContext(CommonTokenStream tokenStream) {
-        Python3Parser parser = new Python3Parser(tokenStream);
+        PythonParser parser = new PythonParser(tokenStream);
         return parser.file_input();
     }
 
-    public void visitFile_input(Python3Parser.File_inputContext ctx) {
+    public void visitFile_input(PythonParser.File_inputContext ctx) {
         ElementIdentifier parentIdentifier = new ElementIdentifier(PathExtractor.extractNameFromPath(ctx), PathExtractor.extractPath(ctx), Type.MODULE);
-        if (ctx.stmt() != null) {
-            for (Python3Parser.StmtContext stmt : ctx.stmt()) {
-                visitStmt(stmt, parentIdentifier);
+        if (ctx.statements() != null) {
+            for (PythonParser.StatementContext stmt : ctx.statements().statement()) {
+                visitStatement(stmt, parentIdentifier);
             }
         }
     }
 
-    public void visitStmt(Python3Parser.StmtContext ctx, ElementIdentifier parentIdentifier) {
+    public void visitStatement(PythonParser.StatementContext ctx, ElementIdentifier parentIdentifier) {
         if (ctx.simple_stmts() != null) {
             visitSimple_stmts(ctx.simple_stmts(), parentIdentifier);
         } else if (ctx.compound_stmt() != null) {
@@ -103,90 +104,91 @@ public class Python3ElementExtractor extends ElementExtractor {
         }
     }
 
-    public void visitSimple_stmts(Python3Parser.Simple_stmtsContext ctx, ElementIdentifier parentIdentifier) {
-        for (Python3Parser.Simple_stmtContext simpleStmt : ctx.simple_stmt()) {
+    public void visitSimple_stmts(PythonParser.Simple_stmtsContext ctx, ElementIdentifier parentIdentifier) {
+        for (PythonParser.Simple_stmtContext simpleStmt : ctx.simple_stmt()) {
             visitSimple_stmt(simpleStmt, parentIdentifier);
         }
     }
 
-    public void visitCompound_stmt(Python3Parser.Compound_stmtContext ctx, ElementIdentifier parentIdentifier) {
-        if (ctx.funcdef() != null) {
-            visitFuncdef(ctx.funcdef(), parentIdentifier);
-        } else if (ctx.classdef() != null) {
-            visitClassdef(ctx.classdef(), parentIdentifier);
-        } else if (ctx.async_stmt() != null && ctx.async_stmt().funcdef() != null) {
-            visitFuncdef(ctx.async_stmt().funcdef(), parentIdentifier);
-        } else if (ctx.decorated() != null) {
-            if (ctx.decorated().classdef() != null) {
-                visitClassdef(ctx.decorated().classdef(), parentIdentifier);
-            } else if (ctx.decorated().funcdef() != null) {
-                visitFuncdef(ctx.decorated().funcdef(), parentIdentifier);
-            } else if (ctx.decorated().async_funcdef() != null && ctx.decorated().async_funcdef().funcdef() != null) {
-                visitFuncdef(ctx.decorated().async_funcdef().funcdef(), parentIdentifier);
-            }
+    public void visitCompound_stmt(PythonParser.Compound_stmtContext ctx, ElementIdentifier parentIdentifier) {
+        if (ctx.function_def() != null) {
+            visitFunction_def(ctx.function_def(), parentIdentifier);
+        } else if (ctx.class_def() != null) {
+            visitClass_def(ctx.class_def(), parentIdentifier);
         }
     }
 
-    public ElementIdentifier visitClassdef(Python3Parser.ClassdefContext ctx, ElementIdentifier parentIdentifier) {
-        if (ctx.name() == null) {
+    public ElementIdentifier visitClass_def(PythonParser.Class_defContext ctx, ElementIdentifier parentIdentifier) {
+        PythonParser.Class_def_rawContext raw = ctx.class_def_raw();
+        if (raw == null || raw.name() == null) {
             return null;
         }
-        String name = ctx.name().getText();
+        String name = raw.name().getText();
         String path = PathExtractor.extractPath(ctx);
-        List<String> childClassOf = getParentClasses(ctx);
+        List<String> childClassOf = getParentClasses(raw);
         ElementIdentifier identifier = new ElementIdentifier(name, path, Type.CLASS);
-        int startLine = ctx.getStart().getLine();
-        int endLine = ctx.getStop().getLine();
+        int startLine = raw.getStart().getLine();
+        int endLine = lastContentLine(raw);
 
-        if (ctx.block() != null && ctx.block().stmt() != null) {
-            for (Python3Parser.StmtContext stmt : ctx.block().stmt()) {
-                visitStmt(stmt, identifier);
-            }
-        }
+        visitBlockStatements(raw.block(), identifier);
 
         addClassElement(name, path, parentIdentifier, childClassOf, startLine, endLine);
         return identifier;
     }
 
-    public ElementIdentifier visitFuncdef(Python3Parser.FuncdefContext ctx, ElementIdentifier parentIdentifier) {
-        if (ctx.name() == null) {
+    public ElementIdentifier visitFunction_def(PythonParser.Function_defContext ctx, ElementIdentifier parentIdentifier) {
+        PythonParser.Function_def_rawContext raw = ctx.function_def_raw();
+        if (raw == null || raw.name() == null) {
             return null;
         }
-        String name = ctx.name().getText();
+        String name = raw.name().getText();
         String path = PathExtractor.extractPath(ctx);
         ElementIdentifier identifier = new ElementIdentifier(name, path, Type.FUNCTION);
-        int startLine = ctx.getStart().getLine();
-        int endLine = ctx.getStop().getLine();
+        int startLine = raw.getStart().getLine();
+        int endLine = lastContentLine(raw);
 
         List<String> calleeNames = new ArrayList<>();
-        if (ctx.block() != null && ctx.block().stmt() != null) {
-            for (Python3Parser.StmtContext stmt : ctx.block().stmt()) {
-                visitStmt(stmt, identifier);
-            }
-            collectCallNamesFromTree(ctx.block(), calleeNames);
+        PythonParser.BlockContext block = raw.block();
+        if (block != null) {
+            visitBlockStatements(block, identifier);
+            collectCallNamesFromTree(block, calleeNames);
         }
         addFunctionElement(name, path, parentIdentifier, startLine, endLine, calleeNames);
         return identifier;
     }
 
-    public void visitSimple_stmt(Python3Parser.Simple_stmtContext ctx, ElementIdentifier parentIdentifier) {
-        if (ctx.expr_stmt() != null) {
-            visitExpr_stmt(ctx.expr_stmt(), parentIdentifier);
+    private void visitBlockStatements(PythonParser.BlockContext block, ElementIdentifier parentIdentifier) {
+        if (block == null) {
+            return;
+        }
+        if (block.statements() != null) {
+            for (PythonParser.StatementContext stmt : block.statements().statement()) {
+                visitStatement(stmt, parentIdentifier);
+            }
+        } else if (block.simple_stmts() != null) {
+            visitSimple_stmts(block.simple_stmts(), parentIdentifier);
+        }
+    }
+
+    public void visitSimple_stmt(PythonParser.Simple_stmtContext ctx, ElementIdentifier parentIdentifier) {
+        if (ctx.assignment() != null) {
+            visitAssignment(ctx.assignment(), parentIdentifier);
         } else if (ctx.import_stmt() != null) {
             visitImport_stmt(ctx.import_stmt());
         }
     }
 
-    private void visitImport_name(Python3Parser.Import_nameContext ctx, List<String> importedNames) {
-        for (Python3Parser.Dotted_as_nameContext dotted : ctx.dotted_as_names().dotted_as_name()) {
+    private void visitImport_name(PythonParser.Import_nameContext ctx, List<String> importedNames) {
+        for (PythonParser.Dotted_as_nameContext dotted : ctx.dotted_as_names().dotted_as_name()) {
             importedNames.add(dotted.dotted_name().getText());
         }
     }
 
-    private void visitImport_from(Python3Parser.Import_fromContext ctx, List<String> importedNames) {
+    private void visitImport_from(PythonParser.Import_fromContext ctx, List<String> importedNames) {
         String base = ctx.dotted_name() != null ? ctx.dotted_name().getText() : "";
-        if (ctx.import_as_names() != null) {
-            for (Python3Parser.Import_as_nameContext name : ctx.import_as_names().import_as_name()) {
+        PythonParser.Import_from_targetsContext targets = ctx.import_from_targets();
+        if (targets != null && targets.import_from_as_names() != null) {
+            for (PythonParser.Import_from_as_nameContext name : targets.import_from_as_names().import_from_as_name()) {
                 importedNames.add(base.isEmpty() ? name.name(0).getText() : base + "." + name.name(0).getText());
             }
         } else if (!base.isEmpty()) {
@@ -194,7 +196,7 @@ public class Python3ElementExtractor extends ElementExtractor {
         }
     }
 
-    public void visitImport_stmt(Python3Parser.Import_stmtContext ctx) {
+    public void visitImport_stmt(PythonParser.Import_stmtContext ctx) {
         List<String> importedNames = new ArrayList<>();
         if (ctx.import_name() != null) {
             visitImport_name(ctx.import_name(), importedNames);
@@ -208,35 +210,35 @@ public class Python3ElementExtractor extends ElementExtractor {
         pendingImportsByPath.get(importPath).addAll(importedNames);
     }
 
-    public void visitExpr_stmt(Python3Parser.Expr_stmtContext ctx, ElementIdentifier parentIdentifier) {
-        if (ctx.ASSIGN() != null && ctx.testlist_star_expr().size() > 1) {
-            extractVariablesFromExprStmt(ctx, parentIdentifier);
+    public void visitAssignment(PythonParser.AssignmentContext ctx, ElementIdentifier parentIdentifier) {
+        if (!ctx.star_targets().isEmpty() && ctx.annotated_rhs() != null) {
+            extractVariablesFromAssignment(ctx, parentIdentifier);
         }
     }
 
-    private List<String> getParentClasses(Python3Parser.ClassdefContext ctx) {
+    private List<String> getParentClasses(PythonParser.Class_def_rawContext ctx) {
         List<String> parentClasses = new ArrayList<>();
-        if (ctx.arglist() != null) {
-            for (Python3Parser.ArgumentContext arg : ctx.arglist().argument()) {
+        if (ctx.arguments() != null && ctx.arguments().args() != null) {
+            for (PythonParser.ExpressionContext arg : ctx.arguments().args().expression()) {
                 parentClasses.add(arg.getText());
             }
         }
         return parentClasses;
     }
 
-    private void extractVariablesFromExprStmt(Python3Parser.Expr_stmtContext ctx, ElementIdentifier parentIdentifier) {
-        if (ctx.testlist_star_expr().size() < 2) {
+    private void extractVariablesFromAssignment(PythonParser.AssignmentContext ctx, ElementIdentifier parentIdentifier) {
+        if (ctx.star_targets().size() != 1) {
             return;
         }
-        List<String> varNames = extractVariableNames(ctx.testlist_star_expr(0));
-        List<String> values = extractVariableNames(ctx.testlist_star_expr(1));
+        List<String> varNames = extractTargetNames(ctx.star_targets(0));
+        List<String> values = extractRhsValues(ctx.annotated_rhs());
         List<String> types = inferTypesFromValues(values);
         String path = PathExtractor.extractPath(ctx);
         int startLine = ctx.getStart().getLine();
         int endLine = ctx.getStop().getLine();
 
         if (varNames.size() != values.size()) {
-            throw new IllegalArgumentException("The number of variable names and values does not match");
+            return;
         }
 
         for (int i = 0; i < varNames.size(); i++) {
@@ -244,13 +246,22 @@ public class Python3ElementExtractor extends ElementExtractor {
         }
     }
 
-    private List<String> extractVariableNames(Python3Parser.Testlist_star_exprContext variableDeclarators) {
+    private List<String> extractTargetNames(PythonParser.Star_targetsContext targets) {
         List<String> variableNames = new ArrayList<>();
-        for (Python3Parser.TestContext testCtx : variableDeclarators.test()) {
-            String name = testCtx.getText();
-            variableNames.add(name);
+        for (PythonParser.Star_targetContext target : targets.star_target()) {
+            variableNames.add(target.getText());
         }
         return variableNames;
+    }
+
+    private List<String> extractRhsValues(PythonParser.Annotated_rhsContext rhs) {
+        List<String> values = new ArrayList<>();
+        if (rhs.star_expressions() != null) {
+            for (PythonParser.Star_expressionContext value : rhs.star_expressions().star_expression()) {
+                values.add(value.getText());
+            }
+        }
+        return values;
     }
 
     private List<String> inferTypesFromValues(List<String> values) {
@@ -294,42 +305,62 @@ public class Python3ElementExtractor extends ElementExtractor {
         elementRegistry.addFunction(function);
     }
 
+    private int lastContentLine(ParseTree tree) {
+        for (int i = tree.getChildCount() - 1; i >= 0; i--) {
+            ParseTree child = tree.getChild(i);
+            if (child instanceof TerminalNode terminal) {
+                if (isLayoutToken(terminal.getSymbol().getType())) {
+                    continue;
+                }
+                return terminal.getSymbol().getLine();
+            }
+            int childLine = lastContentLine(child);
+            if (childLine > 0) {
+                return childLine;
+            }
+        }
+        return 0;
+    }
+
+    private boolean isLayoutToken(int tokenType) {
+        return tokenType == PythonParser.NEWLINE || tokenType == PythonParser.INDENT || tokenType == PythonParser.DEDENT || tokenType == PythonLexer.EOF;
+    }
+
     private void collectCallNamesFromTree(ParseTree tree, List<String> names) {
-        if (tree instanceof Python3Parser.Atom_exprContext atomExpr) {
-            visitAtom_expr(atomExpr, names);
+        if (tree instanceof PythonParser.PrimaryContext primary) {
+            collectCallNameFromPrimary(primary, names);
         }
         for (int i = 0; i < tree.getChildCount(); i++) {
             collectCallNamesFromTree(tree.getChild(i), names);
         }
     }
 
-    private void collectCallNameFromTrailer(Python3Parser.TrailerContext trailer, List<String> names) {
-        if (trailer.name() != null) {
-            names.add(trailer.name().getText());
-        }
-    }
-
-    private void collectCallNameFromAtom(Python3Parser.AtomContext atom, List<String> names) {
-        if (atom.name() != null) {
-            names.add(atom.name().getText());
-        }
-    }
-
-    private void visitAtom_expr(Python3Parser.Atom_exprContext atomExpr, List<String> names) {
-        if (atomExpr.trailer() == null || atomExpr.trailer().isEmpty()) {
+    private void collectCallNameFromPrimary(PythonParser.PrimaryContext primary, List<String> names) {
+        if (!isCallPrimary(primary)) {
             return;
         }
-        List<Python3Parser.TrailerContext> trailers = atomExpr.trailer();
-        for (int i = 0; i < trailers.size(); i++) {
-            Python3Parser.TrailerContext trailer = trailers.get(i);
-            if (trailer.getChildCount() > 0 && "(".equals(trailer.getChild(0).getText())) {
-                if (i > 0) {
-                    collectCallNameFromTrailer(trailers.get(i - 1), names);
-                } else {
-                    collectCallNameFromAtom(atomExpr.atom(), names);
-                }
+        PythonParser.PrimaryContext callee = primary.primary();
+        if (callee == null) {
+            return;
+        }
+        if (callee.name() != null) {
+            names.add(callee.name().getText());
+        } else if (callee.atom() != null && callee.atom().name() != null) {
+            names.add(callee.atom().name().getText());
+        }
+    }
+
+    private boolean isCallPrimary(PythonParser.PrimaryContext primary) {
+        if (primary.primary() == null) {
+            return false;
+        }
+        for (int i = 0; i < primary.getChildCount(); i++) {
+            ParseTree child = primary.getChild(i);
+            if (child instanceof TerminalNode terminal && "(".equals(terminal.getText())) {
+                return true;
             }
         }
+        return false;
     }
 
     private void addClassElement(String name, String path, ElementIdentifier parentIdentifier, List<String> childClassOf, int startLine, int endLine) {
@@ -337,7 +368,7 @@ public class Python3ElementExtractor extends ElementExtractor {
         elementRegistry.addClass(python3ClassElement);
     }
 
-    private void addModules(Python3Parser.File_inputContext ctx) {
+    private void addModules(PythonParser.File_inputContext ctx) {
         Type type = Type.MODULE;
         String name = PathExtractor.extractNameFromPath(ctx);
         String path = PathExtractor.extractPath(ctx);
